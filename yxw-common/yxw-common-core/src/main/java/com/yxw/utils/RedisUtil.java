f5722f3fc8554b5cf8d1ee3c68a47de799588d14
AaNeei <AaaNeei@163.com>
2019-05-03 22:44:51 +0800

    手动事务,解决redis和数据库双向事务问题,同生共死

--- yxw-common/yxw-common-core/src/main/java/com/yxw/utils/RedisUtil.java
+++ yxw-common/yxw-common-core/src/main/java/com/yxw/utils/RedisUtil.java
@@ -15,34 +15,90 @@ import java.util.concurrent.TimeUnit;
  */
 @Component
 public class RedisUtil {
-
-
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
-    public void setString(String key, Object data) {
+    /**
+     * 存放string类型
+     *
+     * @param key     key
+     * @param data    数据
+     * @param timeout 超时间
+     */
+    public void setString(String key, String data, Long timeout) {
+        try {
+
+            stringRedisTemplate.opsForValue().set(key, data);
+            if (timeout != null) {
+                stringRedisTemplate.expire(key, timeout, TimeUnit.SECONDS);
+            }
+
+        } catch (Exception e) {
+
+        }
+
+    }
+
+    /**
+     * 开启Redis 事务
+     * isTransaction
+     *
+     * @param
+     */
+    public void begin() {
+        // 开启Redis 事务权限
+        stringRedisTemplate.setEnableTransactionSupport(true);
+        // 开启事务
+        stringRedisTemplate.multi();
+
+    }
+
+    /**
+     * 提交事务
+     * isTransaction
+     *
+     * @param
+     */
+    public void exec() {
+        // 成功提交事务
+        stringRedisTemplate.exec();
+    }
+
+    /**
+     * 回滚Redis 事务
+     */
+    public void discard() {
+        stringRedisTemplate.discard();
+    }
+
+    /**
+     * 存放string类型
+     *
+     * @param key  key
+     * @param data 数据
+     */
+    public void setString(String key, String data) {
         setString(key, data, null);
     }
 
-    public void setString(String key, Object data, Long timeout) {
-        if (data instanceof String) {
-            String value = (String) data;
-            stringRedisTemplate.opsForValue().set(key, value);
-        } else {
-            String value = data.toString();
-            stringRedisTemplate.opsForValue().set(key, value);
-        }
-        if (timeout != null) {
-            stringRedisTemplate.expire(key, timeout, TimeUnit.SECONDS);
-        }
-    }
-
+    /**
+     * 根据key查询string类型
+     *
+     * @param key
+     * @return
+     */
     public String getString(String key) {
-        return (String) stringRedisTemplate.opsForValue().get(key);
+        String value = stringRedisTemplate.opsForValue().get(key);
+        return value;
     }
 
-    public void delKey(String key) {
-        stringRedisTemplate.delete(key);
-    }
+    /**
+     * 根据对应的key删除key
+     *
+     * @param key
+     */
+    public Boolean delKey(String key) {
+        return stringRedisTemplate.delete(key);
 
+    }
 }
